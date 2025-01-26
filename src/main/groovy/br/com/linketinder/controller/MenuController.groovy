f91a794d2225bbdcf62af168e7f400cd0c8055bb
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-26 14:09:12 -0300

    feat: adicionando try/catch no menu controller

--- src/main/groovy/br/com/linketinder/controller/MenuController.groovy
+++ src/main/groovy/br/com/linketinder/controller/MenuController.groovy
@@ -20,32 +20,35 @@ class MenuController {
     void iniciar() {
         while (true) {
             view.exibirMenu()
+            try {
+                def opcao = scanner.nextLine().toInteger()
 
-            def opcao = scanner.nextLine().toInteger()
-
-            switch (opcao) {
-                case 1:
-                    view.listarCandidatos(model.candidatos)
-                    break
-                case 2:
-                    view.listarEmpresas(model.empresas)
-                    break
-                case 3:
-                    Candidato novoCandidato = view.cadastrarNovoCandidato()
-                    model.candidatos.add(novoCandidato)
-                    println "Candidato cadastrado com sucesso!"
-                    break
-                case 4:
-                    Empresa novaEmpresa = view.cadastrarNovaEmpresa()
-                    model.empresas.add(novaEmpresa)
-                    println "Empresa cadastrada com sucesso!"
-                    break
-                case 5:
-                    println "Saindo do programa. Até mais!"
-                    System.exit(0)
-                    break
-                default:
-                    println "Opção inválida! Tente novamente.\n"
+                switch (opcao) {
+                    case 1:
+                        view.listarCandidatos(model.candidatos)
+                        break
+                    case 2:
+                        view.listarEmpresas(model.empresas)
+                        break
+                    case 3:
+                        Candidato novoCandidato = view.cadastrarNovoCandidato()
+                        model.candidatos.add(novoCandidato)
+                        println "Candidato cadastrado com sucesso!"
+                        break
+                    case 4:
+                        Empresa novaEmpresa = view.cadastrarNovaEmpresa()
+                        model.empresas.add(novaEmpresa)
+                        println "Empresa cadastrada com sucesso!"
+                        break
+                    case 5:
+                        println "Saindo do programa. Até mais!"
+                        System.exit(0)
+                        break
+                    default:
+                        println "Opção inválida! Tente novamente.\n"
+                }
+            } catch (Exception e) {
+                println "Vocẽ digitou algo não númerico, tente novamente."
             }
         }
     }
