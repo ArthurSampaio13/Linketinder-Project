2341777b457e2272c4b79fe4acc2cca32353d8b6
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-26 14:10:21 -0300

    chore: nome no main

--- src/main/groovy/br/com/linketinder/Main.groovy
+++ src/main/groovy/br/com/linketinder/Main.groovy
@@ -5,6 +5,8 @@ import br.com.linketinder.model.Dados
 import br.com.linketinder.view.Menu
 
 static void main(String[] args) {
+    // Arthur Lopes
+
     Dados model = new Dados()
     Menu view = new Menu()
     MenuController controller = new MenuController(model, view)
