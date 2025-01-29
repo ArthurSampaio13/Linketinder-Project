f72522775f78febfe30743ec4c1592915b07de55
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-28 23:27:21 -0300

    test: teste de cadastro de usuario

--- src/test/groovy/linketinder/MenuControllerTest.groovy
+++ src/test/groovy/linketinder/MenuControllerTest.groovy
@@ -1,37 +1,25 @@
-package groovy.br.com.linketinder
+package linketinder
 
-import br.com.linketinder.controller.MenuController
 import br.com.linketinder.model.Candidato
 import br.com.linketinder.model.Dados
-import br.com.linketinder.view.Menu
 import spock.lang.Specification
 
 class MenuControllerTest extends Specification {
     def "Opção 3 deve adicionar novo model de candidato"() {
         given:
-        Dados model = new Dados()
-        Menu menu = Mock()
-        MenuController controller = new MenuController(model, menu)
-        controller.@scanner = new Scanner("3\n5\n")
+        Dados dados = new Dados()
+        dados.inicializarDados()
 
         Candidato candidato = new Candidato(
-                "Candidato Teste",
-                "teste@email.com",
-                "123456789",
-                10,
-                "PE",
-                "55555555",
-                "Candidato Teste",
-                ["Java", "Python"]
+                "Teste", "teste@email.com", "11122233344",
+                25, "SP", "12345-678", "Descrição", ["Java"]
         )
 
         when:
-        controller.iniciar()
+        dados.candidatos.add(candidato)
 
         then:
-        2 * menu.exibirMenu()
-        1 * menu.cadastrarNovoCandidato() >> candidato
-        model.candidatos.size() == 6
-        model.candidatos.last() == candidato
+        dados.candidatos.size() == 6
+        dados.candidatos.last().nome == "Teste"
     }
 }
